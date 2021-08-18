13bd70675941a7fd781028813649654ac54348ae
Ash <sgt16@yandex.ru>
2021-08-18 13:31:24 +0300

    4. Скачивание файла с ограничением. [#144271 #190941]00

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -8,16 +8,18 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String file;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String file) {
         this.url = url;
         this.speed = speed;
+        this.file = file;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(file)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead = 0;
             try {
@@ -43,12 +45,16 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-       if ((args.length != 2)
+       if ((args.length != 3)
                || (args[0].isEmpty())
                || (args[1].isEmpty())) {
            String url = args[0];
            int speed = Integer.parseInt(args[1]);
-           Thread wget = new Thread(new Wget(url, speed));
+           String file = "pom_tmp.xml";
+           if (!args[2].isEmpty()) {
+               file = args[2];
+           }
+           Thread wget = new Thread(new Wget(url, speed, file));
            wget.start();
            wget.join();
        } else {

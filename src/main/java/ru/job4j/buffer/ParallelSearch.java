3fe59fb2814bb0d2f51f2537585a920b977b5855
Ash <sgt16@yandex.ru>
2021-08-24 23:39:18 +0300

    2. Обеспечить остановку потребителя. [#66825]00

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,51 @@
+package ru.job4j.buffer;
+
+import ru.job4j.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        final Thread provider = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            e.printStackTrace();
+                        }
+                    }
+                    try {
+                        Thread.sleep(1000);
+                        Thread.currentThread().interrupt();
+                        System.out.println("STOPP provider");
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+
+                }
+        );
+
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        System.out.println(queue.poll());
+                        System.out.println("AAA");
+                    }
+                    try {
+                        Thread.sleep(2000);
+                        Thread.currentThread().interrupt();
+                        System.out.println("STOPP costomer");
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        consumer.start();
+        provider.start();
+        consumer.join();
+        provider.join();
+    }
+}

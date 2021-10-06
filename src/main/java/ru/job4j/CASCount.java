22b9957e7e2c5c59d17c5852323efc172851cbc0
ASh <sgt16@yandex.ru>
2021-10-06 23:17:16 +0300

    0. CAS - операции [#6859]01

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,54 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public CASCount(int start) { // стартовое значение счётчика
+        this.count.set(start);
+    }
+
+    public void increment() {
+        boolean ok;
+        do {
+            int now = count.get();
+            int next = now + 1;
+            ok = this.count.compareAndSet(now, next);
+        } while (!ok);
+    }
+
+    public int get() {
+        return count.get();
+    }
+
+    /**
+     * Вызовем инкремент по 5 раз из разных потоков
+     */
+    public static void main(String[] args) throws InterruptedException {
+        CASCount casCount = new CASCount(0);
+        Thread first = new Thread(() -> {
+                    for (int i = 0; i < 5; i++) {
+                        casCount.increment();
+                    }
+                }
+        );
+        first.start();
+        Thread second = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                casCount.increment();
+            }
+        }
+        );
+        second.start();
+
+        first.join();
+        second.join();
+        first.interrupt();
+        second.interrupt();
+        System.out.println(casCount.get());
+    }
+}

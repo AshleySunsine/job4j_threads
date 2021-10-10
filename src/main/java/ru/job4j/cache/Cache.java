fd32ccf85786d687ac8dfd4324cd1a0dc5cef16a
ASh <sgt16@yandex.ru>
2021-10-10 19:39:47 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,31 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        memory.computeIfPresent(model.getId(), (integer, base) -> {
+            Integer key = base.getId();
+            Base stored = memory.get(key);
+            if (stored.getVersion() != base.getVersion()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            return memory.replace(key, new Base(base.getId(), base.getVersion() + 1));
+        });
+        return false;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+    public Base get(Integer key) {
+        return memory.getOrDefault(key, null);
+    }
+}
---
+++ src/test/java/ru/job4j/cache/CacheTest.java
@@ -0,0 +1,84 @@
+package ru.job4j.cache;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CacheTest {
+
+    @Test
+    public void add() {
+        Cache cache = new Cache();
+        Base base = new Base(1, 0);
+        cache.add(base);
+
+        Base user1 = cache.get(1);
+        user1.setName("User 1");
+
+        Base user2 = cache.get(1);
+        user1.setName("User 2");
+        cache.add(user1);
+        cache.add(user2);
+        assertThat(cache.get(1), is(user2));
+    }
+
+    @Test
+    public void update() {
+        Cache cache = new Cache();
+        Base base = new Base(1, 0);
+        cache.add(base);
+
+        Base user1 = cache.get(1);
+        user1.setName("User 1");
+        Base user2 = cache.get(1);
+        user1.setName("User 2");
+        Base user3 = cache.get(1);
+        user3.setName("User 3");
+
+        cache.add(user1);
+        cache.add(user2);
+        cache.update(user3);
+        assertThat(cache.get(1), is(user3));
+
+    }
+
+    @Test
+    public void delete() {
+        Cache cache = new Cache();
+        Base base = new Base(1, 0);
+        cache.add(base);
+        cache.delete(base);
+        assertNull(cache.get(1));
+    }
+
+    @Test
+    public void get() {
+        Cache cache = new Cache();
+        Base base = new Base(1, 0);
+        cache.add(base);
+        assertEquals(cache.get(1), base);
+    }
+
+    @Test
+    public void updateWithException() throws OptimisticException, InterruptedException {
+        Cache cache = new Cache();
+        Base base = new Base(1, 0);
+        cache.add(base);
+        Thread first = new Thread( () -> {
+            Base user1 = cache.get(1);
+            user1.setName("User 1");
+            cache.update(user1);
+        });
+        Thread second = new Thread( () -> {
+            Base user1 = cache.get(1);
+            user1.setName("User 222");
+            cache.update(user1);
+        });
+       first.start();
+       second.start();
+       first.join();
+       second.join();
+
+    }
+}

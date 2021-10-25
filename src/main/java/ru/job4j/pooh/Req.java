5121cbc7db7e9ca5c545021806421fd6ea8b1ef8
ASh <sgt16@yandex.ru>
2021-10-25 21:56:50 +0300

    2. Тестовое задание - проект "Pooh JMS" [#268841]

---
+++ src/main/java/ru/job4j/pooh/Req.java
@@ -0,0 +1,58 @@
+package ru.job4j.pooh;
+
+import java.util.Arrays;
+
+public class Req {
+
+    private final String httpRequestType;
+    private final String poohMode;
+    private final String sourceName;
+    private final String param;
+
+    public Req(String httpRequestType, String poohMode, String sourceName, String param) {
+        this.httpRequestType = httpRequestType;
+        this.poohMode = poohMode;
+        this.sourceName = sourceName;
+        this.param = param;
+    }
+
+    public static Req of(String content) {
+        String httpRequestType;
+        String poohMode;
+        String sourceName;
+        String param;
+        String[] lines = content.split(System.lineSeparator());
+        String[] firstLine = lines[0].split(" ");
+        String[] modeAndName;
+        modeAndName = firstLine[1].split("/");
+        httpRequestType = firstLine[0];
+        poohMode = modeAndName[1];
+        sourceName = modeAndName[2];
+        param = lines[lines.length - 1];
+        if ("GET".equals(httpRequestType)) {
+            if (modeAndName.length < 4) {
+                param = "";
+            } else {
+                param = modeAndName[3];
+            }
+        }
+
+        return new Req(httpRequestType, poohMode, sourceName, param);
+    }
+
+    public String httpRequestType() {
+        return httpRequestType;
+    }
+
+    public String getPoohMode() {
+        return poohMode;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public String getParam() {
+        return param;
+    }
+}

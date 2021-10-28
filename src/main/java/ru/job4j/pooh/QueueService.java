d8367390471a41bc3fb849335cbecccd47339de8
ASh <sgt16@yandex.ru>
2021-10-28 14:00:43 +0300

    2. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,6 +1,11 @@
 package ru.job4j.pooh;
 
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class QueueService implements Service {
+    private static ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
     @Override
     public Resp process(Req req) {
         if ("POST".equals(req.httpRequestType())) {
@@ -14,23 +19,36 @@ public class QueueService implements Service {
 
     private Resp post(Req req) {
         try {
-            var l = CASMap.putIfAbsent(req);
+            var l = putIfAbsent(req);
             if (null != l) {
-                CASMap.put(req);
+                put(req);
             }
         } catch (Exception e) {
             return new Resp(e.toString(), "500");
         }
-        System.out.println(CASMap.queue.get(req.getSourceName()));
+        System.out.println(queue.get(req.getSourceName()));
         return new Resp(req.getParam() + " added to" + req.getSourceName(), "200");
     }
 
     private Resp get(Req req) {
-        var l = CASMap.extract(req);
+        var l = extract(req);
         if (!l.isEmpty()) {
             System.out.println(l);
             return new Resp(l, "200");
         }
         return new Resp("", "500");
+    }
+
+    private ConcurrentLinkedQueue<String> putIfAbsent(Req req) {
+        return queue.putIfAbsent(req.getSourceName(),
+                new ConcurrentLinkedQueue<>(Collections.singleton(req.getParam())));
+    }
+
+    private void put(Req req) {
+        queue.get(req.getSourceName()).add(req.getParam());
+    }
+
+    private String extract(Req req) {
+        return queue.getOrDefault(req.getSourceName(), new ConcurrentLinkedQueue<>()).poll();
     }
 }
--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -1,20 +1,45 @@
 package ru.job4j.pooh;
 
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class TopicService implements Service {
+
+    private final ConcurrentHashMap<String,
+            ConcurrentHashMap<String, ConcurrentLinkedQueue<String>>> queueTopic = new ConcurrentHashMap<>();
+
     @Override
     public Resp process(Req req) {
         if ("POST".equals(req.httpRequestType())) {
-            CASMap.topicPutIfAbsent(req);
+            topicPutIfAbsent(req);
             return new Resp("ok", "200");
         }
         if ("GET".equals(req.httpRequestType())) {
-        String ex = CASMap.topicExtract(req);
-        if (ex == null) {
-            return new Resp("PODPISKA", "200");
+        String ex = topicExtract(req);
+        if (null == ex || ex.equals(req.getParam())) {
+            return new Resp("", "200");
         } else {
             return new Resp(ex, "200");
         }
         }
         return null;
+    }
+
+    private void topicPutIfAbsent(Req req) {
+        queueTopic.forEach((key, value) -> value.put(req.getSourceName(), new ConcurrentLinkedQueue<>(Collections.singleton(req.getParam()))));
+    }
+
+    private String topicExtract(Req req) {
+        return queueTopic.computeIfAbsent(req.getParam(),
+                k -> createValue(req)).get(req.getSourceName()).poll();
+    }
+
+    private ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> createValue(Req req) {
+        ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> person = new ConcurrentHashMap<>();
+        ConcurrentLinkedQueue<String> newQueue = new ConcurrentLinkedQueue<>();
+        newQueue.add(req.getParam());
+        person.putIfAbsent(req.getSourceName(), newQueue);
+        return person;
     }
 }
